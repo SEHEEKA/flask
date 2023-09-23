22ca58c565100d57a8dd20fb0eb179f2ce7f1f51
Seheeka <86560213+SEHEEKA@users.noreply.github.com>
2023-09-23 12:00:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,9 @@ def first_webpage():
     name="Flask"
     return render_template('index.html',index_variable=name)
 
+@app.route("/flask_2")
+def second_flask():
+    return "Python is fun!"
+
+#run using debug argument
 app.run(debug=True)
